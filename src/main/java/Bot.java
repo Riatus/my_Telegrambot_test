59fa5f11998c0e2cc327758a2a4a5558d9f14022
Riatus <poddubnovvlad@gmail.com>
2022-12-10 13:09:01 +0300

    add sends a response with a picture to the user

---
+++ src/main/java/Bot.java
@@ -0,0 +1,74 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.GetFile;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.PhotoSize;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.io.*;
+import java.net.URL;
+
+public class Bot extends TelegramLongPollingBot {
+
+    @Override
+    public String getBotUsername() {
+        return "vladsecond_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+
+        return "5400873082:AAHeFdplQHynztdZ0zCTRJpG1LA_lX6qpuM";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+        String response = message.getDate().toString();
+        PhotoSize photoSize = message.getPhoto().get(0);
+        final String fileId = photoSize.getFileId();
+        try {
+            final org.telegram.telegrambots.meta.api.objects.File file = sendApiMethod(new GetFile(fileId));
+            final String imageUrl = "https://api.telegram.org/file/bot" + getBotToken() + "/" + file.getFilePath();
+            saveImage(imageUrl, "received_image.jpeg");
+        } catch (TelegramApiException | IOException e) {
+            e.printStackTrace();
+        }
+
+
+        //
+        System.out.println(message.getText());
+        SendPhoto sendPhoto = new SendPhoto();
+        sendPhoto.setChatId(message.getChatId());
+        InputFile newFile = new InputFile();
+        newFile.setMedia(new File("ты крыса.png"));
+        sendPhoto.setPhoto(newFile);
+        sendPhoto.setCaption("да ");
+
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(message.getChatId());
+        sendMessage.setText("Your message: " + response);
+        try {
+            execute(sendMessage);
+            execute(sendPhoto);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void saveImage(String url, String fileName) throws IOException {
+        URL urlModel = new URL(url);
+        InputStream inputStream = urlModel.openStream();
+        OutputStream outputStream = new FileOutputStream(fileName);
+        byte[] b = new byte[2048];
+        int length;
+        while ((length = inputStream.read(b)) != -1) {
+            outputStream.write(b, 0, length);
+        }
+        inputStream.close();
+        outputStream.close();
+    }
+}
